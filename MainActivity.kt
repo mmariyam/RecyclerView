b924c852eb87f949dfd2d24be885e91e0b25d688
Mariyam Mukhumova <75296517+mmariyam@users.noreply.github.com>
2023-04-25 13:46:14 +0200

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,38 @@
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.neco_desarrollo.plantshandbook.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    private val adapter = PlantAdapter()
+    private val imageIdList = listOf(
+        R.drawable.plant1,
+        R.drawable.plant2,
+        R.drawable.plant3,
+        R.drawable.plant4,
+        R.drawable.plant5,
+    )
+    private var index = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
+    }
+
+    private fun init() {
+        binding.apply {
+            rcView.layoutManager = GridLayoutManager(this@MainActivity, 3)
+            rcView.adapter = adapter
+            buttonAdd.setOnClickListener {
+                if(index > 4) index = 0
+                val plant = Plant(imageIdList[index], "Plant $index")
+                adapter.addPlant(plant)
+                index++
+            }
+        }
+    }
+}
